b5e2c7f3daa515595805c4d32536c6ae4d9b910e
Sunmukh <shanmukhgupta7@gmail.com>
2026-06-04 17:42:57 +0530

    feat-added_category_page

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from unicodedata import category
 
+from django.shortcuts import  render, redirect, get_object_or_404
+from django.http import HttpResponse
+from blogs.models import Blog, Category
 # Create your views here.
+
+def posts_by_category(request, category_id):
+    # Fetch the posts that belongs to the category with the id category_id
+    posts = Blog.objects.filter(status='Published', category = category_id)
+    # try:
+    #     category = Category.objects.get(pk=category_id)
+        
+    # except:
+    # # redirect to home page if category does not exist
+    #     return redirect ('home')
+    
+    category = get_object_or_404(Category, pk=category_id)
+
+
+
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'posts_by_category.html', context)
+
+
